46bdc20074c4972f04744e7c7fe4beb226250d53
tanvira1993 <tanvira1993@gmail.com>
2019-02-24 22:42:20 +0600

    some edit

--- public/ng-assets/js/controllers/userRegController.js
+++ public/ng-assets/js/controllers/userRegController.js
@@ -1,42 +1,37 @@
-/* Setup blank page controller */
 angular.module('EcommerceApp').controller('userRegController', ['$scope', '$rootScope', '$location', '$timeout', '$http', 
 	function($scope, $rootScope, $location, $timeout, $http) {
 		$scope.$on('$viewContentLoaded', function() {
-			
+			$scope.createUser = function(){
 
-        	//Create User Account
-        	$scope.createUser = function(){
+				$http({
+					method: 'post',
+					url: 'api/createUser',
+					data:$scope.userInfo
+				}).then(function (response) {
+					$scope.userInfo=null;        			
+					$scope.userRegistrationForm.$setPristine();
 
-        		
-        		$http({
-        			method: 'post',
-        			url: 'api/createUser',
-        			data:$scope.userInfo
-        		}).then(function (response) {
-        			$scope.userInfo=null;        			
-        			$scope.userRegistrationForm.$setPristine();
+					swal({
+						title: 'Success!',
+						text: 'Account Created Successfully.',
+						type: 'success'
+					}, function () {
 
-        			swal({
-        				title: 'Success!',
-        				text: 'Account Created Successfully.',
-        				type: 'success'
-        			}, function () {
+						$location.path("/login");
 
-        				$location.path("/login");
+						if (!$scope.$$phase)
+							$scope.$apply();
+					});
 
-        				if (!$scope.$$phase)
-        					$scope.$apply();
-        			});
+				}, function (response) {
 
-        		}, function (response) {
-        			
-        			swal({
-        				title: response.data.heading,
-        				text: response.data.message,
-        				html:true,
-        				type: 'error'
-        			});
-        		});
-        	}
-        });
+					swal({
+						title: response.data.heading,
+						text: response.data.message,
+						html:true,
+						type: 'error'
+					});
+				});
+			}
+		});
 	}]);
